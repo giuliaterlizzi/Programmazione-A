1ad4831f9d8d39143c0c4697493daef8da94f92f
Giulia Terlizzi <giuliaterlizzi925@gmail.com>
2025-10-28 18:49:38 +0100

    esercizi

---
+++ esercizi/numeri10a1.cpp
@@ -0,0 +1,17 @@
+/*Scrivi un programma che stampa i
+ numeri da 10 a 1 usando un ciclo while. */
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    
+    int i =10;
+    while (i >= 1) {
+        cout<< i<< " ";
+        i--;
+    }
+    
+
+    return 0;
+}
---
+++ esercizi/numeripari.cpp
@@ -0,0 +1,16 @@
+//Scrivi un programma che stampa  solo i numeri pari (10, 8, 6, …).
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    
+    int i=10;
+    while (i >=2 ) {
+        cout<< i<< " ";
+        i= i-2;
+    }
+    
+
+    return 0;
+}
