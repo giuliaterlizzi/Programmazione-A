9fc2fda5873d138cc7354f87a8d89c31b39881c3
Giulia Terlizzi <giuliaterlizzi925@gmail.com>
2025-11-07 18:17:51 +0100

    esercizi

---
+++ esercizi/asterischi.cpp
@@ -0,0 +1,24 @@
+/*Stampare 10 volte *****
+                    *****
+                    *****
+                    *****
+                    *****
+                    *****
+                    *****
+                    *****
+                    *****
+                    ***** */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int c = 0;
+	while  (c < 10) {
+		cout << "*****" << endl;
+		c++;
+	}
+
+    return 0;
+}
---
+++ esercizi/quadrati.cpp
@@ -0,0 +1,18 @@
+//stampare i quadrati dei primi 5 interi >= 0 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+     cout << "I quadrati dei primi 5 interi sono:";
+
+    int i = 0;
+    while(i < 5){
+        cout<< " "<< i * i;
+        i++;
+    }
+
+
+    return 0;
+}
